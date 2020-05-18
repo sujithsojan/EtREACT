0560387a23bafe21fb6474d3ab0d56cc764fa036
Chelsa <charleschelsa@gmail.com>
2020-05-18 22:43:10 +0530

    Card text colour change

--- src/components/Api_Expense.js
+++ src/components/Api_Expense.js
@@ -9,18 +9,33 @@ class Expense extends Component{
             };
       
     componentDidMount(){
+        var date, parts;
+        let months={"01":"January","02":"February","03":"March","04":"April","05":"May","06":"June",
+        "07":"July","08":"August","09":"September","10":"October","11":"November","12":"December",};
         console.log(this.props.dataA)
         if(this.props.dataA){
-            axios.get("http://localhost:8081/tracker/register/getExpense",{ params: {uSER_ID:this.props.message}})
+            axios.get("http://localhost:8081/tracker/register/getIncome",{ params: {uSER_ID:this.props.message}})
             .then(res => {
+                for (var key in res.data) {
+                    parts=res.data[key].DATE.split("-");
+                    res.data[key].DATE = parts[2]+' '+months[parts[1]]+', '+parts[0]
+                    res.data[key].AMOUNT = res.data[key].AMOUNT.toFixed(2);
+                    //res.data[key]["colour"]="#F35B8C";
+                }
             this.setState({items: res.data});
             });
             console.log(this.state.items.DATE);
         }
         
         if(this.props.dataB){
-            axios.get("http://localhost:8081/tracker/register/getIncome",{ params: {uSER_ID:this.props.message}})
+            axios.get("http://localhost:8081/tracker/register/getExpense",{ params: {uSER_ID:this.props.message}})
             .then(res => {
+                for (var key in res.data) {
+                    parts=res.data[key].DATE.split("-");
+                    res.data[key].DATE = parts[2]+' '+months[parts[1]]+', '+parts[0]
+                    res.data[key].AMOUNT = res.data[key].AMOUNT.toFixed(2);
+                    res.data[key]["colour"]="#F35B8C";
+                }
             this.setState({items: res.data});
             });
         }
@@ -28,6 +43,12 @@ class Expense extends Component{
         if(this.props.dataBoth){
             axios.get("http://localhost:8081/tracker/register/getIncomeExpense",{ params: {uSER_ID: this.props.message}})
             .then(res => {
+                for (var key in res.data) {
+                    parts=res.data[key].DATE.split("-");
+                    res.data[key].DATE = parts[2]+' '+months[parts[1]]+', '+parts[0]
+                    res.data[key].AMOUNT = res.data[key].AMOUNT.toFixed(2);
+                    res.data[key]["colour"]="#F35B8C";
+                }
             this.setState({items: res.data});
             });
         }
